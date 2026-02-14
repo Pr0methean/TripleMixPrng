e54d42f5275037f1bacb90fe25a5786b9a6b4660
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-02-13 20:14:47 -0800

    Add CLI argument to specify seed

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+use std::env;
 use std::io::{stdout, Write};
 use rand::rngs::SysRng;
 use rand_core::{Rng, SeedableRng, TryRng, UnwrapErr};
 use triple_mix_prng::TripleMixPrng;
 
 fn main() {
+    let args: Vec<_> = env::args_os().collect();
     let mut seed = [0u8; TripleMixPrng::SEED_SIZE];
-    SysRng.try_fill_bytes(&mut seed).unwrap();
+    if let Some(seed_arg) = args.get(1) && let Ok(decoded_seed) = hex::decode(seed_arg.as_encoded_bytes()) {
+        seed[0..(TripleMixPrng::SEED_SIZE.min(decoded_seed.len()))].copy_from_slice(&decoded_seed);
+    } else {
+        SysRng.try_fill_bytes(&mut seed).unwrap();
+    }
     eprintln!("Seed: {}", seed.map(|b| format!("{:02X}", b)).join(""));
     let mut prng = TripleMixPrng::from_rng(&mut UnwrapErr(SysRng));
     loop {
