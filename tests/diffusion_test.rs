e757eda4d9f8e2c3fbd1e1072c9bd89a31e25102
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-02-15 20:20:05 -0800

    Generalize seed-diffusion test to apply to all bits

--- tests/diffusion_test.rs
+++ tests/diffusion_test.rs
@@ -5,17 +5,21 @@ use triple_mix_prng::TripleMixPrng;
 
 #[test]
 fn test_seed_diffusion() {
-    let mut seed = [0u8; 256];
+    let seed = [0u8; TripleMixPrng::SEED_SIZE];
     let mut rng1 = TripleMixPrng::from_seed(GenericArray::from(seed));
     let start_val1 = rng1.try_next_u64().unwrap();
 
-    // Change the VERY LAST byte of the seed.
-    // In the old implementation (incremental absorb), this would NOT affect the first lane
-    // because the first lane was generated after only absorbing the first 64 bytes.
-    seed[255] = 1;
-    let mut rng2 = TripleMixPrng::from_seed(GenericArray::from(seed));
-    let start_val2 = rng2.try_next_u64().unwrap();
+    // In the old implementation (incremental absorb), changing the LAST byte would NOT affect the
+    // first lane, because the first lane was generated after only absorbing the first 64 bytes.
+    for byte_index in 0..TripleMixPrng::SEED_SIZE {
+        for bit_index in 0..=7 {
+            let mut seed = [0u8; TripleMixPrng::SEED_SIZE];
+            seed[byte_index] = 1 << bit_index;
+            let mut rng2 = TripleMixPrng::from_seed(GenericArray::from(seed));
+            let start_val2 = rng2.try_next_u64().unwrap();
 
-    assert_ne!(start_val1, start_val2, "Changing the last byte of the seed did not affect the first output! Diffusion failure.");
-    assert_ne!(start_val1, 0, "Output shouldn't be zero");
+            assert_ne!(start_val1, start_val2, "Changing byte {byte_index} bit {bit_index} of the seed did not affect the first output! Diffusion failure.");
+            assert_ne!(start_val1, 0, "Output shouldn't be zero");
+        }
+    }
 }
