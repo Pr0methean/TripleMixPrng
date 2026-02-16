c2fc3773d72e39fefead6e8078e35543aa7a3bd5
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-02-16 13:50:23 -0800

    Set SIMD_WIDTH back to 4 (larger is slower because it needs SIMD_WIDTH Feistel rounds)

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![feature(likely_unlikely, portable_simd)]
 
-use typenum::{U, U64};
+use typenum::{U};
 use core::convert::Infallible;
 use generic_array::{typenum, GenericArray};
 use rs_shake256::{Shake256Hasher};
@@ -13,7 +13,6 @@ use rand_core::block::{BlockRng, Generator};
 use std::simd::*;
 use std::simd::num::SimdUint;
 use std::simd::cmp::{SimdPartialEq, SimdPartialOrd};
-use generic_array::typenum::op;
 use rand::RngExt;
 use rand_core::utils::read_words;
 
@@ -48,7 +47,7 @@ impl TripleMixPrng {
         TripleMixPrng(BlockRng::new(core))
     }
 }
-const SIMD_WIDTH: usize = 8;
+const SIMD_WIDTH: usize = 4;
 const OUTPUT_LEN: usize = 4 * SIMD_WIDTH;
 type Simd64 = Simd<u64, SIMD_WIDTH>;
 const ONES: Simd64 = Simd64::splat(1);
@@ -201,19 +200,13 @@ impl Generator for TripleMixSimdCore {
         const TINYMT64_SH2: Simd64 = Simd64::splat(32);
         const TINYMT64_SH8: Simd64 = Simd64::splat(8);
 
-        // These constants are taken from the hexadecimal expansion of pi, but with the
+        // These constants are taken from the hexadecimal expansion of pi * 1<<252, but with the
         // most significant bit set to one on lanes 0 and 2 (it'd otherwise always be zero).
-        // 79216d5d98979fb1bd1310ba698dfb5ac2ffd72dbd01adfb7b8e1afed6a267e96ba7c9045f12c7f9924a19947b3916cf70801f2e2858efc16636920d871574e69a458fea3f4933d7e0d95748f728eb658718bcd5882154aee7b54a41dc25a59b59c30d5392af26013c5d1b023286085f0ca417918b8db38ef8e79dcb0603a180e6c9e0e8bb01e8a3ed71577c1bd314b2778af2fda55605c60e65525f3aa55ab945748986263e8144055ca396a2aab10b6b4cc5c341141e8cea15486af7c72e993b3ee1411636fbc2a2ba9c55d741831f6ce5c3e169b87931eafd6ba336c24cf5c7a325381289586773b8f48986b4bb9afc4bfe81b6628219361d809ccfb21a991487cac605dec8032ef845d5de98575b1dc262302eb651b8823893e81d396acc50f6d6ff383f442392e0b4482a484200469c8f04a9e1f9b5e21c66842f6e96c9a670c9c61abd388f06a51a0d2d8542f68960fa728ab5133a36eef0b6c137a3be4ba3bf0507efb2a98a1f1651d39af017666ca593e82430e888cee8619456f9fb47d84a5c33b8b5ebee06f75d885c12073401a449f56c16aa64ed3aa62363f77061bfedf72429b023d37d0d724d00a1248db0fead349f1c09b075372c980991..._16
         const LANE_CONSTANTS: Simd64 = Simd64::from_array(
-        [0x3243_f6a8_885a_308d,
+        [0xd243_f6a8_885a_308d,
             0x3131_98a2_e037_0734,
-            0x4a40_9382_2299_f31d,
-            0x0082_efa9_8ec4_e6c8,
-            0x9452_821e_638d_0137,
-            0x7be5_466c_f34e_90c6,
-            0xcc0a_c29b_7c97_c50d,
-            0xd3f8_4d5b_5b54_7091
-        ]
+            0xca40_9382_2299_f31d,
+            0x0082_efa9_8ec4_e6c8]
         );
         let mut xr0 = self.xr0;
         let mut xr1 = self.xr1;
@@ -291,11 +284,11 @@ impl Generator for TripleMixSimdCore {
                 cur_r0 ^= h0;
                 cur_r1 ^= h1;
 
-                match r%4 {
-                    0 => { cur_r1 = simd_swizzle!(cur_r1, [1, 3, 5, 7, 0, 2, 4, 6]); }
-                    1 => { cur_r0 = simd_swizzle!(cur_r0, [4, 5, 6, 7, 0, 1, 2, 3]); }
-                    2 => { cur_l1 = simd_swizzle!(cur_l1, [0, 4, 2, 6, 5, 1, 7, 3]); }
-                    3 => { cur_l0 = simd_swizzle!(cur_l0, [7, 6, 5, 4, 3, 2, 1, 0]); }
+                match r {
+                    0 => { cur_r1 = simd_swizzle!(cur_r1, [3, 0, 1, 2]); }
+                    1 => { cur_r0 = simd_swizzle!(cur_r0, [2, 3, 0, 1]); }
+                    2 => { cur_l1 = simd_swizzle!(cur_l1, [1, 2, 3, 0]); }
+                    3 => { cur_l0 = simd_swizzle!(cur_l0, [3, 2, 1, 0]); }
                     _ => unsafe { unreachable_unchecked() }
                 }
             }
