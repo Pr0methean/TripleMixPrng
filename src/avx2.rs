4bc37f8010e8d3ad67eb7cfa95e924bfaf1a885c
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-02-18 17:04:26 -0800

    Refactor: don't need to mask out upper halves in mullo_u64x4_avx2

--- src/avx2.rs
+++ src/avx2.rs
@@ -26,17 +26,12 @@ pub fn mullo_const(a: Simd64, c: u64) -> Simd64 {
 #[inline(always)]
 unsafe fn mullo_u64x4_avx2(a: __m256i, b: __m256i) -> __m256i {
     unsafe {
-        // low64(a*b) = (a_lo*b_lo) + ((a_hi*b_lo + a_lo*b_hi) << 32)  (mod 2^64)
-        let lo_mask = _mm256_set1_epi64x(0xFFFF_FFFFu64 as i64);
-
-        let a_lo = _mm256_and_si256(a, lo_mask);
-        let b_lo = _mm256_and_si256(b, lo_mask);
         let a_hi = _mm256_srli_epi64(a, 32);
         let b_hi = _mm256_srli_epi64(b, 32);
 
-        let p0 = _mm256_mul_epu32(a_lo, b_lo);
-        let p1 = _mm256_mul_epu32(a_hi, b_lo);
-        let p2 = _mm256_mul_epu32(a_lo, b_hi);
+        let p0 = _mm256_mul_epu32(a, b);
+        let p1 = _mm256_mul_epu32(a_hi, b);
+        let p2 = _mm256_mul_epu32(a, b_hi);
 
         let p12 = _mm256_add_epi64(p1, p2);
         let p12_shift = _mm256_slli_epi64(p12, 32);
