6c50b8973119b4dbff55672d2796d1d3372bf5d8
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-02-24 12:20:40 -0800

    Remove stack_sizes (doesn't work on Windows)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 #![feature(portable_simd)]
 
-use std::fs::File;
-use std::io::Read;
 use genetic_algorithm::crossover::CrossoverUniform;
 use genetic_algorithm::fitness::FitnessOrdering;
 use genetic_algorithm::genotype::{Genotype, ListGenotype};
@@ -10,14 +8,9 @@ use genetic_algorithm::select::SelectElite;
 use genetic_algorithm::strategy::evolve::EvolveReporterSimple;
 use genetic_algorithm::strategy::prelude::Evolve;
 use genetic_algorithm::strategy::Strategy;
-use stack_sizes::analyze_executable;
 use triple_mix_prng::{build_list_of_instructions, PrngMixingFitness, TripleMixSimdCore};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut program_file = File::open("triple_mix_simd.exe")?;
-    let mut program_bytes = Vec::with_capacity(program_file.metadata()?.len() as usize);
-    program_file.read_to_end(&mut program_bytes)?;
-    analyze_executable(&program_bytes)?;
     simple_log::console("debug")?;
     let genotype = ListGenotype::builder()
         .with_allele_list(build_list_of_instructions(TripleMixSimdCore::NUM_OPERANDS))
@@ -29,7 +22,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     .with_select(SelectElite::new(0.5, 0.02))         // sort the chromosomes by fitness to determine crossover order. Strive to replace 50% of the population with offspring. Allow 2% through the non-generational best chromosomes gate before selection and replacement
     .with_crossover(CrossoverUniform::new(0.7, 0.8))  // crossover all individual genes between 2 chromosomes for offspring with 70% parent selection (30% do not produce offspring) and 80% chance of crossover (20% of parents just clone)
     .with_mutate(MutateSingleGene::new(0.2))          // mutate offspring for a single gene with a 20% probability per chromosome
-    .with_fitness(PrngMixingFitness)                          // count the number of true values in the chromosomes
+    .with_fitness(PrngMixingFitness)
+    .with_fitness_cache(1024)
     .with_fitness_ordering(FitnessOrdering::Maximize) // optional, default is Maximize, aim towards the most true values
     .with_target_population_size(64)                 // evolve with 100 chromosomes
     .with_target_fitness_score(-100)                   // goal is 100 times true in the best chromosome
