3cd12080f59fd1ae7010a77d47d543834c5eb689
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-02-24 20:37:37 -0800

    Tweak: combine hill_climb output with a random population

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![feature(portable_simd)]
 
+use std::iter::repeat_with;
 use genetic_algorithm::crossover::CrossoverUniform;
 use genetic_algorithm::fitness::FitnessOrdering;
 use genetic_algorithm::genotype::{Genotype, MultiListGenotype};
@@ -7,11 +8,13 @@ use genetic_algorithm::mutate::MutateSingleGene;
 use genetic_algorithm::select::SelectElite;
 use genetic_algorithm::strategy::evolve::EvolveReporterSimple;
 use genetic_algorithm::strategy::hill_climb::HillClimbVariant;
-use genetic_algorithm::strategy::prelude::{Evolve, HillClimbBuilder};
+use genetic_algorithm::strategy::prelude::{Evolve, HillClimbBuilder, HillClimbReporterSimple};
 use genetic_algorithm::strategy::Strategy;
 use log::info;
 use triple_mix_prng::{build_input_instructions, build_list_of_instructions, build_output_instructions, Instruction, PrngMixingFitness};
 
+const TOTAL_POPULATION: usize = 1 << 10;
+const HILL_CLIMBED_POPULATION: usize = 1 << 8;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let body_operands = build_list_of_instructions(false);
     simple_log::console("debug")?;
@@ -67,18 +70,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 build_output_instructions(15),
         ])
         .with_genes_size(48);
+    let genotype = genotype_builder.clone().build().unwrap();
+    let mut rng = ga_rand::thread_rng();
+    let random_population: Vec<_> = repeat_with(|| genotype.chromosome_constructor_random(&mut rng).genes)
+        .take(TOTAL_POPULATION - HILL_CLIMBED_POPULATION)
+        .collect();
     let (best_hill_climb, hill_climbs) = HillClimbBuilder::new()
-        .with_genotype(genotype_builder.clone().build().unwrap())
+        .with_genotype(genotype)
         .with_variant(HillClimbVariant::Stochastic)
         .with_fitness(PrngMixingFitness)
         .with_par_fitness(true)
-        .with_max_stale_generations(32)
-        .call_par_repeatedly(32)
+        .with_max_stale_generations(8)
+        .with_reporter(HillClimbReporterSimple::new(4))
+        .call_par_repeatedly(HILL_CLIMBED_POPULATION)
         .unwrap();
     let (best_genes, best_fitness_score) = best_hill_climb.best_genes_and_fitness_score().unwrap();
     info!("Bootstrap results: Score: {best_fitness_score:?}\n\rGenes:\n\r{best_genes:?}");
     let bootstrapped_genotype = genotype_builder.with_seed_genes_list(
-        hill_climbs.into_iter().flat_map(|hc| hc.best_genes()).collect()
+        hill_climbs.into_iter().flat_map(|hc| hc.best_genes())
+            .chain(random_population).collect()
     ).build().unwrap();
     let evolve = Evolve::<MultiListGenotype<Instruction>,_,_,_,_,_,_>::builder()
     .with_genotype(bootstrapped_genotype)
