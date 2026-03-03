d17841c47eb2b2cf19421806094506195e8f3d6a
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-03-03 13:35:53 -0800

    Set up benchmarks in CI, using `criterion-hypothesis`

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,6 +1,7 @@
 use criterion::measurement::Measurement;
 use criterion::{BenchmarkId, Criterion, Throughput, criterion_group, criterion_main};
 use criterion_cycles_per_byte::CyclesPerByte;
+#[cfg(feature = "bench_include_threadrng")]
 use rand::rng;
 use rand::rngs::SysRng;
 use rand_core::{Rng, SeedableRng, TryRng};
@@ -13,6 +14,7 @@ fn fill_bytes<T: Measurement>(c: &mut Criterion<T>) {
     let mut triple_mix = TripleMixPrng::<NotReproducible>::from(&seed);
     let mut triple_mix_reproducible = TripleMixPrng::<SameEndianness>::from(&seed);
     let mut triple_mix_x_reproducible = TripleMixPrng::<CrossPlatform>::from(&seed);
+    #[cfg(feature = "bench_include_threadrng")]
     let mut thread_rng = rng();
 
     // Allocate buffer as u64's so that it's aligned
@@ -43,6 +45,7 @@ fn fill_bytes<T: Measurement>(c: &mut Criterion<T>) {
                 black_box(&*misaligned_buffer);
             })
         });
+        #[cfg(feature = "bench_include_threadrng")]
         group.bench_function("ThreadRng", |b| {
             b.iter(|| {
                 thread_rng.fill_bytes(misaligned_buffer);
@@ -59,6 +62,7 @@ fn next_u64<T: Measurement>(c: &mut Criterion<T>) {
     let mut triple_mix = TripleMixPrng::<NotReproducible>::from(&seed);
     let mut triple_mix_reproducible = TripleMixPrng::<SameEndianness>::from(&seed);
     let mut triple_mix_x_reproducible = TripleMixPrng::<CrossPlatform>::from(&seed);
+    #[cfg(feature = "bench_include_threadrng")]
     let mut thread_rng = rng();
     let mut group = c.benchmark_group("next_u64");
     group.throughput(Throughput::Bytes(size_of::<u64>() as u64));
@@ -69,6 +73,7 @@ fn next_u64<T: Measurement>(c: &mut Criterion<T>) {
     group.bench_function("TripleMixPrng with CrossPlatform reproducibility", |b| {
         b.iter(|| triple_mix_x_reproducible.next_u64())
     });
+    #[cfg(feature = "bench_include_threadrng")]
     group.bench_function("ThreadRng", |b| b.iter(|| thread_rng.next_u64()));
     group.finish();
 }
