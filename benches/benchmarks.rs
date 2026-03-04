1bd770c779f870e0de84380f972a0df6c783a0c1
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-03-03 16:52:19 -0800

    Fix? Restore ThreadRng benchmarks so we can't time out waiting for them

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -2,7 +2,6 @@ use criterion::measurement::Measurement;
 use criterion::{BenchmarkId, Criterion, Throughput, criterion_group, criterion_main};
 #[cfg(any(target_os = "linux", target_arch = "x86_64", target_arch = "x86"))]
 use criterion_cycles_per_byte::CyclesPerByte;
-#[cfg(feature = "bench_include_threadrng")]
 use rand::rng;
 use rand::rngs::SysRng;
 use rand_core::{Rng, SeedableRng, TryRng};
@@ -15,7 +14,6 @@ fn fill_bytes<T: Measurement>(c: &mut Criterion<T>) {
     let mut triple_mix = TripleMixPrng::<NotReproducible>::from(&seed);
     let mut triple_mix_reproducible = TripleMixPrng::<SameEndianness>::from(&seed);
     let mut triple_mix_x_reproducible = TripleMixPrng::<CrossPlatform>::from(&seed);
-    #[cfg(feature = "bench_include_threadrng")]
     let mut thread_rng = rng();
 
     // Allocate buffer as u64's so that it's aligned
@@ -46,7 +44,6 @@ fn fill_bytes<T: Measurement>(c: &mut Criterion<T>) {
                 black_box(&*misaligned_buffer);
             })
         });
-        #[cfg(feature = "bench_include_threadrng")]
         group.bench_function("ThreadRng", |b| {
             b.iter(|| {
                 thread_rng.fill_bytes(misaligned_buffer);
@@ -63,7 +60,6 @@ fn next_u64<T: Measurement>(c: &mut Criterion<T>) {
     let mut triple_mix = TripleMixPrng::<NotReproducible>::from(&seed);
     let mut triple_mix_reproducible = TripleMixPrng::<SameEndianness>::from(&seed);
     let mut triple_mix_x_reproducible = TripleMixPrng::<CrossPlatform>::from(&seed);
-    #[cfg(feature = "bench_include_threadrng")]
     let mut thread_rng = rng();
     let mut group = c.benchmark_group("next_u64");
     group.throughput(Throughput::Bytes(size_of::<u64>() as u64));
@@ -74,7 +70,6 @@ fn next_u64<T: Measurement>(c: &mut Criterion<T>) {
     group.bench_function("TripleMixPrng with CrossPlatform reproducibility", |b| {
         b.iter(|| triple_mix_x_reproducible.next_u64())
     });
-    #[cfg(feature = "bench_include_threadrng")]
     group.bench_function("ThreadRng", |b| b.iter(|| thread_rng.next_u64()));
     group.finish();
 }
