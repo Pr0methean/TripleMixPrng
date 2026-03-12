0e4bf92fc36d60bacf8e53880d20f284ed36494f
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-03-12 12:05:16 -0700

    Optimizations to feed a whole number of blocks into CSHAKE both before and after cloning in `permute()` (#54)
    * Optimize DEFAULT_SEED_SIZE, and provide LARGE_SEED_SIZE for seeding from extra-high-entropy sources
    
    * Fix: update `get_random_seed` return size in examples/common/mod.rs to match LARGE_SEED_SIZE
    
    * Fix: can't use `from_seed` in examples since it takes `DEFAULT_SEED_SIZE`, so use `from` instead, and add test for `from_rng`
    
    * Remove unused imports in examples
    
    * Update test_cross_platform_reproducibility for updated seed size
    
    * Correct seed-size calculations, because Kmac::new and CShake::new call fill_block
    
    * Optimization: let permute() start without a part-full block by adding 1 more seed byte, and let it skip an almost-empty block by folding round into tweak
    
    * Optimization: pad all seeds to a size in whole SHA3 blocks
    
    * Optimization: pad input from `fork_with_domain_separation` as well
    
    * Optimization: get padding by using up to 3 of `remaining_results`
    
    * Fix: generate random padding if buffer is empty, to ensure the internal state advances
    
    * Fix: need `rand` dependency for `RngExt::fill`
    
    * Lower thresholds to p=.005 for test_byte_frequencies and test_u16_frequencies
    
    * Fix unused-import warning for Rng
    
    * Sort sizes
    
    * Specify `rand` version as a workspace dependency for DRY
    
    * Fix: need to include rand in regular dependencies
    
    * Fix: need `rand` feature `thread_rng` for tests

--- examples/endless_bytes.rs
+++ examples/endless_bytes.rs
@@ -1,9 +1,9 @@
 use crate::common::get_random_seed;
 use aws_lc_rs::test::from_hex;
-use rand_core::{Rng, SeedableRng};
+use rand_core::Rng;
 use rand_triplemix::TripleMixPrng;
 use rand_triplemix::reproducibility::NotReproducible;
-use rand_triplemix::seed::DEFAULT_SEED_SIZE;
+use rand_triplemix::seed::LARGE_SEED_SIZE;
 use std::env::args_os;
 use std::ffi::OsString;
 use std::io::{Write, stdout};
@@ -18,15 +18,15 @@ fn main() {
         && let Some(seed_arg_utf8) = seed_arg.to_str()
         && let Ok(decoded_seed) = from_hex(seed_arg_utf8)
     {
-        let mut seed = [0u8; DEFAULT_SEED_SIZE];
-        seed[0..(DEFAULT_SEED_SIZE.min(decoded_seed.len()))].copy_from_slice(&decoded_seed);
+        let mut seed = [0u8; LARGE_SEED_SIZE];
+        seed[0..(LARGE_SEED_SIZE.min(decoded_seed.len()))].copy_from_slice(&decoded_seed);
         eprintln!("Seed: {}", seed.map(|b| format!("{:02X}", b)).join(""));
-        prng = TripleMixPrng::from_seed(seed.into());
+        prng = TripleMixPrng::from(seed);
     } else if args.get(1) == Some(&OsString::from_str("z").unwrap()) {
         prng = TripleMixPrng::almost_all_zeroes_state();
     } else {
         let seed = get_random_seed();
-        prng = TripleMixPrng::from_seed(seed.into());
+        prng = TripleMixPrng::from(seed);
     }
     loop {
         let mut buffer = [0u8; 1 << 16];
